408f600502c1c9d62a34154b28d483991000054d
ceejaymar <martinezcj2@gmail.com>
2025-08-17 23:16:56 -0400

    improve a11y

--- src/components/experience/Experience.tsx
+++ src/components/experience/Experience.tsx
@@ -60,7 +60,7 @@ const ExperienceSection = styled(motion.div)`
   `}
 `;
 
-const Title = styled.h4`
+const Title = styled.h2`
   font-size: 1rem;
   font-weight: 500;
   color: ${({ theme }) => theme.colors.primary};
